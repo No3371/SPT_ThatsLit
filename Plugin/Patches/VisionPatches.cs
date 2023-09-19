5cc6ba51b77a9d97b87fe5d836b4daa4ebf6d4c5
BA <developer@3371.online>
2023-09-19 15:05:34 +0800

    Make bots unaffected for a moment after it has vision

--- Plugin/Patches/VisionPatches.cs
+++ Plugin/Patches/VisionPatches.cs
@@ -47,7 +47,7 @@ namespace ThatsLit.Patches.Vision
             var disFactor = Mathf.Clamp01((dis - 10) / 100f);
             // To scale down various sneaking bonus
             // The bigger the distance the bigger it is, capped to 110m
-            disFactor = disFactor * disFactor * 0.8f; // A slow accelerating curve, 110m => 1, 10m => 0 (Scaled down to make things imperfect)
+            disFactor = disFactor * disFactor * 0.8f; // A slow accelerating curve, 110m => 1, 10m => 0 (Then scaled down to make things imperfect)
 
             if (__instance.Owner.WeaponManager.ShootController.IsAiming)
             {
@@ -147,6 +147,8 @@ namespace ThatsLit.Patches.Vision
                 else if (factor < -0.2f) __result *= 1 - factor * ThatsLitPlugin.DarknessImpactScale.Value;
                 else if (factor < 0f) __result *= 1 - factor / 1.5f * ThatsLitPlugin.DarknessImpactScale.Value;
                 else if (factor > 0f) __result /= (1 + factor / 2f * ThatsLitPlugin.BrightnessImpactScale.Value);
+
+                __result = Mathf.Lerp(__result, original, Mathf.Clamp01((1f - Time.time - __instance.PersonalSeenTime) / 0.1f)); // just seen (0s) => original, 0.1s => modified
 
                 __result += ThatsLitPlugin.FinalOffset.Value;
                 if (__result < 0.001f) __result = 0.001f;
