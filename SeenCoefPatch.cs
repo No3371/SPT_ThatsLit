5932587b9814627b2641308f5a6d446e3fb51cf0
BA <developer@3371.online>
2023-11-22 00:20:17 +0800

    Tweaked fog blocking

--- SeenCoefPatch.cs
+++ SeenCoefPatch.cs
@@ -131,15 +131,15 @@ namespace ThatsLit
                 // disFactorLong = Mathf.Lerp(0, disFactorLong, sinceSeen / (8f * (1.2f - disFactorLong)) / (isGoalEnemy ? 0.33f : 1f)); // Takes 1.6 seconds out of visual for the disFactor to reset for AIs at 110m away, 9.6s for 10m, 8.32s for 50m, if it's targeting the player, 3x the time
 
 
-                if (mainPlayer.fog > 0) __result *= 1 + 5 * (dis - 10f) / 10f * Mathf.Clamp01(mainPlayer.fog / 0.1f);
+                if (mainPlayer.fog > 0) __result *= 1 + 10 * (dis - 10f) / 10f * Mathf.Clamp01(mainPlayer.fog / 0.1f);
                 // Considering 0.1 fogginess blocks 10m+ view in 3.7+
                 // 0m  @0.087f -> 1x
                 // 10m  @0.087f -> 1x
-                // 15m  @0.087f -> 1.435x
-                // 100m @0.087f -> 8.83x
+                // 15m  @0.087f -> 5.35x
+                // 100m @0.087f -> 79.3x
                 // 10m  @0.012f -> 1x
-                // 15m  @0.012f -> 1.006x
-                // 100m @0.012f -> 1.108x
+                // 15m  @0.012f -> 1.012x
+                // 100m @0.012f -> 1.216x
 
 
                 // Vector3 EyeToEnemyHead = mainPlayer.MainPlayer.MainParts[BodyPartType.body].Position - __instance.Owner.GetPlayer.MainParts[BodyPartType.head].Position;
