c1136a0ef5cf09bfef0d5ce4b3844ba1c75c39a9
BA <developer@3371.online>
2024-05-24 03:52:28 +0800

    Debug Info

--- src/PlayerDebugInfo.cs
+++ src/PlayerDebugInfo.cs
@@ -7,13 +7,15 @@ namespace ThatsLit
     {
         public bool IsBushRatting { get; set;} 
         public float lastCalcFrom, lastCalcTo, lastScore, lastFactor1, lastFactor2, rawTerrainScoreSample;
-        public int calced = 0, calcedLastFrame = 0, encounter, vagueHint;
+        public int calced = 0, calcedLastFrame = 0, encounter, vagueHint, vagueHintCancel, signalDanger;
         public Vector3 lastTriggeredDetailCoverDirNearest;
         public float lastTiltAngle, lastRotateAngle, lastDisFactorNearest;
         public float lastNearest;
         public float lastFinalDetailScoreNearest;
         internal float scoreRawBase, scoreRaw0, scoreRaw1, scoreRaw2, scoreRaw3, scoreRaw4;
         internal float shinePixelsRatioSample, highLightPixelsRatioSample, highMidLightPixelsRatioSample, midLightPixelsRatioSample, midLowLightPixelsRatioSample, lowLightPixelsRatioSample, darkPixelsRatioSample;
+        internal float lastEncounterShotAngleDelta, lastEncounteringShotCutoff;
+        internal float lastBushRat;
         
     }
 }
