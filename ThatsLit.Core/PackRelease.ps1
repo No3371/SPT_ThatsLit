16bff51ea06137c0b7c12bf34d0da94eb5a7f0eb
BA <developer@3371.online>
2024-05-30 19:59:26 +0800

    Misc

--- ThatsLit.Core/PackRelease.ps1
+++ ThatsLit.Core/PackRelease.ps1
@@ -1,5 +1,8 @@
+$from = $pwd
+cd $PSScriptRoot
 Get-Content ".\ThatsLitPlugin.cs" | Select-String 'public const string ModVersion = "([0-9.]+)"' | ForEach-Object {
     $v = $_.Matches[0].Groups[1].Value
   }
 Write-Host($v)
-Bandizip.exe c -root:BepInEx\plugins\ThatsLit "ThatsLit_${v}.zip" ..\..\BepInEx\plugins\ThatsLit\*
+Bandizip.exe c -root:BepInEx\plugins\ThatsLit "ThatsLit_${v}.zip" ..\..\..\BepInEx\plugins\ThatsLit\*.thatslitcompat.json ..\..\..\BepInEx\plugins\ThatsLit\ThatsLit.Core.dll
+cd $from
