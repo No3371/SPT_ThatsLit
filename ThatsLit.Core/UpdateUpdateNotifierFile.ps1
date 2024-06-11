b8e4b4a02cfed74fcacc8842473971d86773a95b
BA <developer@3371.online>
2024-06-11 22:38:49 +0800

    Fix BOM in .update_notifier by Microsoft

--- ThatsLit.Core/UpdateUpdateNotifierFile.ps1
+++ ThatsLit.Core/UpdateUpdateNotifierFile.ps1
@@ -3,5 +3,5 @@ cd $PSScriptRoot
 Get-Content ".\ThatsLitPlugin.cs" | Select-String 'public const string ModVersion = "([0-9.]+)"' | ForEach-Object {
     $v = $_.Matches[0].Groups[1].Value
   }
-$v | Out-File -FilePath ".update_notifier" -Encoding utf8 -Force
+[IO.File]::WriteAllLines((Resolve-Path ".update_notifier"), $v)
 cd $from
