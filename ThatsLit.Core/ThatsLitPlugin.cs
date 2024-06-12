b27545a01b68c1563ff963a2f0f5fc9cff6f5ffa
BA <developer@3371.online>
2024-06-13 02:55:03 +0800

    Blind Fire Patch enabled by default

--- ThatsLit.Core/ThatsLitPlugin.cs
+++ ThatsLit.Core/ThatsLitPlugin.cs
@@ -210,7 +210,7 @@ namespace ThatsLit
             IncludeBosses              = Config.Bind(category, "Include Bosses", false, "Should all features from this mod work for boss. Makes bosses EASY.");
             EnableEquipmentCheck         = Config.Bind(category, "Equipment Check", true, "Whether the mod checks your equipments. Disabling this stops lights/lasers detection and makes stealth EASY.");
             InterruptSAINNoBush              = Config.Bind(category, "Interrupt SAIN No Bush", false, "DO NOT COMPLAIN ABOUT NO BUSH ESP TO Solarint IF YOU HAVE THIS ON.New SAIN No Bush is designed to be aggressive. It can block bot vision even if you are just 2m away and the bot is looking straight at you. This add a chance to turn off SAIN's No Bush ESP at close range.");
-            ForceBlindFireScatter              = Config.Bind(category, "Force Blind Fire Scatter", false, "Force a random scatter on bot blind fireing, scaled by distance.");
+            ForceBlindFireScatter              = Config.Bind(category, "Force Blind Fire Scatter", true, "Force a random scatter on bot blind fireing, scaled by distance.");
             
         }
 
