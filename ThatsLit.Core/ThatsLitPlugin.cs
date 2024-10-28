426781a9665f30c30fad59d1a47397f2811a2590
BA <developer@3371.online>
2024-10-29 01:26:54 +0800

    1.394.06

--- ThatsLit.Core/ThatsLitPlugin.cs
+++ ThatsLit.Core/ThatsLitPlugin.cs
@@ -25,7 +25,7 @@ namespace ThatsLit
         public const int TarkovVersion = 30626;
         public const string EscapeFromTarkov = "EscapeFromTarkov.exe";
         public const string ModName = "That's Lit";
-        public const string ModVersion = "1.394.05";
+        public const string ModVersion = "1.394.06";
         public const string SPTGUID = "com.SPT.core";
         public const string SPTVersion = "3.9.0";
         private static long modVersionComparable;
